fea330def583c396b0e37d33fd85eb17a45e9906
Damián Cesar Silva <damiancesarsilva16032005@gmail.com>
2026-03-26 20:50:10 +0000

    funciona

--- decrypt.c
+++ decrypt.c
@@ -1,10 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
 
-int main(int argc, char *argv[])
-{
-    // Agregar código aquí.
+int main() {
+    char buffer[8];
+    ssize_t n;
 
-    // Termina la ejecución del programa.
-    exit(EXIT_SUCCESS);
+    while (1) {
+        n = read(STDIN_FILENO, buffer, 8);
+
+        if (n == 0) {
+            // EOF normal
+            break;
+        }
+
+        if (n < 0) {
+            perror("read");
+            exit(EXIT_FAILURE);
+        }
+
+        if (n != 8) {
+            fprintf(stderr, "Error: datos encriptados corruptos (bloque incompleto)\n");
+            exit(EXIT_FAILURE);
+        }
+
+        // escribir el byte real (último byte)
+        ssize_t written = 0;
+        while (written < 1) {
+            ssize_t w = write(STDOUT_FILENO, &buffer[7] + written, 1 - written);
+
+            if (w < 0) {
+                perror("write");
+                exit(EXIT_FAILURE);
+            }
+
+            written += w;
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
--- encrypt.c
+++ encrypt.c
@@ -1,10 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
 
-int main(int argc, char *argv[])
-{
-    // Agregar código aquí.
+int main(int argc, char *argv[]) {
+    srand(time(NULL));
 
-    // Termina la ejecución del programa.
-    exit(EXIT_SUCCESS);
+    char buffer[1];
+    ssize_t n;
+
+    // Caso 1: argumento por línea de comandos
+    if (argc > 1) {
+        char *msg = argv[1];
+        for (int i = 0; i < strlen(msg); i++) {
+
+            // 7 bytes aleatorios
+            for (int j = 0; j < 7; j++) {
+                char r = rand() % 256;
+                if (write(STDOUT_FILENO, &r, 1) != 1) {
+                    perror("write");
+                    exit(1);
+                }
+            }
+
+            // byte real
+            if (write(STDOUT_FILENO, &msg[i], 1) != 1) {
+                perror("write");
+                exit(1);
+            }
+        }
+    }
+    // Caso 2: stdin
+    else {
+        while ((n = read(STDIN_FILENO, buffer, 1)) > 0) {
+
+            for (int j = 0; j < 7; j++) {
+                char r = rand() % 256;
+                if (write(STDOUT_FILENO, &r, 1) != 1) {
+                    perror("write");
+                    exit(1);
+                }
+            }
+
+            if (write(STDOUT_FILENO, buffer, 1) != 1) {
+                perror("write");
+                exit(1);
+            }
+        }
+
+        if (n < 0) {
+            perror("read");
+            exit(1);
+        }
+    }
+
+    return 0;
 }
