bed5901273e817aa744206c539b4390f8d7654fb
JoaqQ1 <quirogajuaco7@gmail.com>
2026-04-01 10:05:04 -0300

    Solucion propuesta de mi parte

--- decrypt.c
+++ decrypt.c
@@ -1,10 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
+#define ENCRYPTED_BLOCK_SIZE 8
+
+int read_full(int fd, char *buffer,int size){
+    int total = 0;
+    while (total < size) {
+        ssize_t n = read(fd, buffer + total, size - total);
+        if (n == 0) return total; // EOF
+        if (n < 0) return -1;    // Error
+        total += n;
+    }
+    return total;
+}
 int main(int argc, char *argv[])
 {
-    // Agregar código aquí.
+    char buffer[ENCRYPTED_BLOCK_SIZE];
 
-    // Termina la ejecución del programa.
+    // Agregar código aquí.
+    while (1) {
+        int total_read = read_full(STDIN_FILENO, buffer, ENCRYPTED_BLOCK_SIZE);
+
+        if (total_read == 0) // EOF fin del archivo
+            break;
+        if (total_read < 0) { // Error en la lectura
+            perror("read");
+            exit(EXIT_FAILURE);
+        }
+        if(total_read != ENCRYPTED_BLOCK_SIZE) { // Lectura de bytes incompleta
+            fprintf(stderr,"Error: datos encriptados corruptos\n");
+            exit(EXIT_FAILURE);
+        }
+
+        if(write(STDOUT_FILENO,&buffer[7],1) < 0 )
+            exit(EXIT_FAILURE);
+    
+    }
     exit(EXIT_SUCCESS);
 }
--- encrypt.c
+++ encrypt.c
@@ -1,10 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+
+#define RANDOM_PADDING_SIZE 7
+#define MAX_NUM_ASCII 255
+
+static void write_random_byte(char byte){
+    
+    for(int i = 0; i < RANDOM_PADDING_SIZE; i++){
+        char random_byte = (char) (rand() % (MAX_NUM_ASCII + 1));
+        if(write(STDOUT_FILENO, &random_byte,1) != 1){
+            perror("write");
+            exit(EXIT_FAILURE);
+        }
+    }
+    if(write(STDOUT_FILENO,&byte,1)!=1){
+            perror("write");
+            exit(EXIT_FAILURE);
+    }
+}
 
 int main(int argc, char *argv[])
 {
     // Agregar código aquí.
-
+    srand(time(NULL));
+    
+    if(argc > 0){
+        for(int i = 1;i < argc;i++){
+            int messageLength = strlen(argv[i]);
+            for(int j = 0; j < messageLength;j++)
+                write_random_byte(argv[i][j]);
+            if(i < argc - 1)
+                write_random_byte(' ');
+        }
+    }
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
 }
