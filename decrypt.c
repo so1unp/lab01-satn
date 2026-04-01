98cdf99ed8e3aeac10b066e8fcd94583d32da739
Ezequiel <ezequielramos22106@gmail.com>
2026-04-01 16:48:09 -0300

    Implementación robusta de decrypt.c con manejo de errores y buffers

--- decrypt.c
+++ decrypt.c
@@ -2,18 +2,37 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
+#include <errno.h>
 
 int main() {
-    char entrada;
+    char bloque_entrada[8];
+    while (1) {
+        ssize_t n = read(STDIN_FILENO, bloque_entrada, 8);
 
-    char basura;
-    while (read(0, &basura, 1)) {
-        for (int i = 0; i < 6; i++) {
-            read(0, &basura, 1);
+        if (n == 0) break;
+        
+        if (n < 0) {
+            perror("Fallo en el read.");
+            exit(EXIT_FAILURE);
         }
-        read(0, &entrada, 1);
-        write(1, &entrada, 1);
+
+        if (n != 8) {
+            fprintf(stderr, "Error, no se leyeron 8 bytes.\n");
+            exit(EXIT_FAILURE);
+        }
+        
+        ssize_t escritos = 0;
+        while (escritos < 1) {  
+            size_t restante = (size_t)(1 - escritos);
+            ssize_t r = write(STDOUT_FILENO, &bloque_entrada[7] + escritos, restante);
+            if (r < 0) {
+                perror("Fallo en el write.");
+                exit(EXIT_FAILURE);
+            }  
+
+            escritos += r;
+        }
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
