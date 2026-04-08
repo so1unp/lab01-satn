df314986188806d6fc4e3c5d36d55541cc475342
Ezequiel <ezequielramos22106@gmail.com>
2026-04-08 14:30:48 -0300

    Se da por finalizado el encrypt y se agregan archivos de prueba strace

--- encrypt.c
+++ encrypt.c
@@ -4,123 +4,53 @@
 #include <time.h>
 
 int main(int argc, char *argv[]) {
-    srand(time(NULL));
+    srand((unsigned int)time(NULL));
 
-    char entrada;
+    char buffer[1];
 
     if (argc > 1) {   
         for (int i = 0; argv[1][i] != '\0'; i++) {
             for (int j = 0; j < 7; j++) {
                 int basura = rand();
-                write(1, &basura, 1);
+                if(write(STDOUT_FILENO, &basura, 1) != 1) {
+                    perror("No se escribió solo 1 byte.");
+                    exit(EXIT_FAILURE);
+                }
             }
-         
-            write(1, &argv[1][i], 1);
+            
+            if(write(STDOUT_FILENO, &argv[1][i], 1) != 1) {
+                perror("No se escribió solo 1 byte.");
+                exit(EXIT_FAILURE);
+            }
         }
     } else {
-        int resultado = read(0, &entrada, 1);
-        while (resultado > 0) {        
+        ssize_t n = read(STDIN_FILENO, buffer, 1);
+        if (n < 0) {
+            perror("Error al leer.");
+            exit(EXIT_FAILURE);
+        }
+        while (n > 0) {        
             for (int i = 0; i < 7; i++) {
                 int basura = rand();
-                write(1, &basura, 1);
+                if(write(STDOUT_FILENO, &basura, 1) != 1) {
+                    perror("No se escribió solo 1 byte.");
+                    exit(EXIT_FAILURE);
+                }
             }
             
-            write(1, &entrada, 1);
+            if(write(STDOUT_FILENO, buffer, 1) != 1) {
+                perror("No se escribió solo 1 byte.");
+                exit(EXIT_FAILURE);
+            }
             
-            resultado = read(0, &entrada, 1);
+            n = read(STDIN_FILENO, buffer, 1);
+
+            if (n < 0) {
+                perror("Error al leer.");
+                exit(EXIT_FAILURE);
+            }  
         }
     }
 
     return 0;
 }
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <time.h>
-
-int main(int argc, char *argv[]) {
-    // Convertir time_t a unsigned int para srand
-    srand((unsigned int)time(NULL));
-
-    char buffer[1];
-    ssize_t n;
-
-    // Caso 1: argumento por línea de comandos
-    if (argc > 1) {
-        char *msg = argv[1];
-        size_t msg_len = strlen(msg);  // Usar size_t para la longitud
-        
-        for (size_t i = 0; i < msg_len; i++) {
-            // 7 bytes aleatorios
-            for (int j = 0; j < 7; j++) {
-                char r = (char)(rand() % 256);  // Conversión explícita
-                if (write(STDOUT_FILENO, &r, 1) != 1) {
-                    perror("write");
-                    exit(1);
-                }
-            }
-
-            // byte real
-            if (write(STDOUT_FILENO, &msg[i], 1) != 1) {
-                perror("write");
-                exit(1);
-            }
-        }
-    }
-    // Caso 2: stdin
-    else {
-        while ((n = read(STDIN_FILENO, buffer, 1)) > 0) {
-            for (int j = 0; j < 7; j++) {
-                char r = (char)(rand() % 256);  // Conversión explícita
-                if (write(STDOUT_FILENO, &r, 1) != 1) {
-                    perror("write");
-                    exit(1);
-                }
-            }
-
-            if (write(STDOUT_FILENO, buffer, 1) != 1) {
-                perror("write");
-                exit(1);
-            }
-        }
-        
-        if (n < 0) {
-            perror("read");
-            exit(1);
-        }
-    }
-
-    return 0;
-}
-*/
